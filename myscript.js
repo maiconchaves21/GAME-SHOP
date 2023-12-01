bb7d9b711fb368fd4a7afb7b0fa120a69223c39a
Maiquinhokkj <141333370+maiconchaves21@users.noreply.github.com>
2023-12-01 00:27:02 +0000

    ok

--- myscript.js
+++ myscript.js
@@ -1,26 +1,18 @@
-function validarompra() {
+function validarCompra() {
+  const form = document.getElementById("checkoutForm");
+  const formData = new FormData(form);
 
-    var nome = document.getElementById('nome').value;
-    var email = document.getElementById('email').value;
-    var cpf = document.getElementById('cpf').value;
-    var telefone = document.getElementById('telefone').value;
-    var cep = document.getElementById('cep').value;
-
-
-    if (nome && email && cpf && telefone && cep) {
-
-        enviarEmail(nome, email, cpf, telefone, cep);
-
-
-        registrarVenda();
-
-
-        alert('Compra concluída com sucesso!');
-
-        document.getElementById('checkoutForm').reset();
-    } else {
-        alert('Por favor, preencha todos os campos obrigatórios.');
-    }
+  fetch("http://jkorpela.fi/cgi-bin/echo.cgi", {
+    method: "POST",
+    body: formData,
+  })
+    .then(response => response.text())
+    .then(data => {
+      console.log(data); 
+    })
+    .catch(error => {
+      console.error("Erro ao enviar o formulário:", error);
+    });
 }
 
 let carrinho = [];
@@ -59,23 +51,4 @@ function finalizarCompra() {
     atualizarTotal(); 
   }
 
-  function validarCompra() {
-    // Lógica de validação (se necessário)
-    
-    // Obtenha os dados do formulário
-    const form = document.getElementById("checkoutForm");
-    const formData = new FormData(form);
   
-    // Use a função fetch para enviar os dados para o servidor
-    fetch("http://jkorpela.fi/cgi-bin/echo.cgi", {
-      method: "POST",
-      body: formData,
-    })
-      .then(response => response.text())
-      .then(data => {
-        console.log(data); // Exibe a resposta do servidor no console (pode ser removido em produção)
-      })
-      .catch(error => {
-        console.error("Erro ao enviar o formulário:", error);
-      });
-  }
